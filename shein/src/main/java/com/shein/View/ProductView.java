eb9dbe197d26b589249b9643a74eab18c211e892
BrunaMelido <bruna_melido@hotmail.com>
2024-04-29 01:40:28 -0300

    Commit Do Dia

--- shein/src/main/java/com/shein/View/ProductView.java
+++ shein/src/main/java/com/shein/View/ProductView.java
@@ -51,18 +51,70 @@ public class ProductView {
     }
 
     public Produto AtualizarEstoque() {
-        Produto produto = new Produto();
-        System.out.println("sku: ");
-        String skuInput = scanner.nextLine();
-        produto.setSku(skuInput);
-        System.out.println("Digite a nova quantidade em estoque:");
-        String size = scanner.nextLine();
-        produto.setSize(size);
-        produto.getUrl();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do produto:");
+        String nome = scanner.nextLine();
+        Produto _produto = new Produto();
+        // while (_eperation != 0){
+        //     _eperation = ProductView.MenuEscolha(null);
+        //     if(info == 2) _produto = _produtoView.ConsultarProduto();
+        //     if(info == 3) _produto = _produtoView.AtualizarEstoque();
+        //     if(info == 0)  System.out.println("Saindo...");
+        //     else System.out.println("Opção inválida.");
+        // // }
+        // if (_produto != null) {
+        System.out.println("Qual informação deseja alterar? (url, name, sku, price, size, brand, description, images)");
+        String info = scanner.nextLine();
+
+        // switch (info) {
+            if(info == "1") System.out.println("Digite a nova URL:");
+            _produto.setUrl(scanner.nextLine());
+            if(info == "2") System.out.println("Digite o novo nome:");
+                _produto.setName(scanner.nextLine());
+                if(info == "3") System.out.println("Digite o novo SKU:");
+                _produto.setSku(scanner.nextLine());
+                if(info == "4") System.out.println("Digite o novo preço:");
+                _produto.setSize(scanner.nextLine());
+                if(info == "5") System.out.println("Digite o novo tamanho:");
+                _produto.setBrand(scanner.nextLine());
+                if(info == "6") System.out.println("Digite a nova marca:");
+                _produto.setBrand(scanner.nextLine());
+            //}
+                // case "6":
+                //     System.out.println("Digite a nova marca:");
+                //     produto.setBrand(scanner.nextLine());
+                //     break;
+                // case "7":
+                //     System.out.println("Digite a nova descrição:");
+                //     produto.setDescription(scanner.nextLine());
+                //     break;
+                // case "8":
+                //     System.out.println("Digite as novas imagens (separadas por vírgula):");
+                //     produto.setImages(scanner.nextLine());
+                //     break;
+                // default:
+                //     System.out.println("Opção inválida.");
+            
+
+            return _produto;
+        }
+        // } else {
+        //     System.out.println("Produto não encontrado.");
+        //     return null;
+        // }
+
+
+        // Produto produto = new Produto();
+        // System.out.println("sku: ");
+        // String skuInput = scanner.nextLine();
+        // produto.setSku(skuInput);
+        // System.out.println("Digite a nova quantidade em estoque:");
+        // String size = scanner.nextLine();
+        // produto.setSize(size);
+        // produto.getUrl();
 
         //colocar todos os valores que forem passados
-        return produto;
-    }
+        
 
     public static int MenuEscolha(String[] args) {
         Scanner scanner = new Scanner(System.in);
